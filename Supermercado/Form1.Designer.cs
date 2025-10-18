a89ff853bcc9427082ae16858ccc8d01f454afb1
Isu-682 <padillamachayariana@gmail.com>
2025-10-17 21:30:06 -0600

    Agregar archivos de proyecto.

---
+++ Supermercado/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Supermercado
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(290, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CLIENTES";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(290, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "COMPRAS CLIENTES";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(290, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "EMPLEADOS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(25, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(290, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "FACTURAS";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 263);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(290, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "PRODUCTOS";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(25, 313);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(290, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "PROVEEDORES";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(25, 363);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(290, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "VENTAS";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(25, 413);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(290, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "VENTAS PRODUCTOS";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(25, 213);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(290, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "FACTURAS DETALLES";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 449);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "frmMenu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
