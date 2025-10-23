e44660d44365c6c3ec2d1797a6b2b127df02e4cc
Isu-682 <padillamachayariana@gmail.com>
2025-10-23 16:32:10 -0600

    Proveedores terminado , provado para revisar y testearlo bien

--- Supermercado/View/frmProveedores.Designer.cs
+++ Supermercado/View/frmProveedores.Designer.cs
@@ -28,21 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.txtbEmpresa = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtbTipo_producto = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtbDireccion = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.txtbNro_tel_sec = new System.Windows.Forms.TextBox();
-            this.txtbNro_tel_princ = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.txtbEmail = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.dgvProveedores = new System.Windows.Forms.DataGridView();
             this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.mtxtbNro_tel_sec = new System.Windows.Forms.MaskedTextBox();
+            this.mtxtbNro_tel_princ = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripEditar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripEliminar = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).BeginInit();
+            this.cmsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtbEmpresa
@@ -93,38 +99,6 @@
             this.label9.TabIndex = 39;
             this.label9.Text = "Direccion";
             // 
-            // txtbNro_tel_sec
-            // 
-            this.txtbNro_tel_sec.Location = new System.Drawing.Point(12, 260);
-            this.txtbNro_tel_sec.Name = "txtbNro_tel_sec";
-            this.txtbNro_tel_sec.Size = new System.Drawing.Size(241, 20);
-            this.txtbNro_tel_sec.TabIndex = 44;
-            // 
-            // txtbNro_tel_princ
-            // 
-            this.txtbNro_tel_princ.Location = new System.Drawing.Point(12, 211);
-            this.txtbNro_tel_princ.Name = "txtbNro_tel_princ";
-            this.txtbNro_tel_princ.Size = new System.Drawing.Size(241, 20);
-            this.txtbNro_tel_princ.TabIndex = 43;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(9, 239);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(154, 13);
-            this.label7.TabIndex = 42;
-            this.label7.Text = "Numero Telefonico Secundario";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 190);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(140, 13);
-            this.label6.TabIndex = 41;
-            this.label6.Text = "Numero Telefonico Principal";
-            // 
             // txtbEmail
             // 
             this.txtbEmail.Location = new System.Drawing.Point(12, 309);
@@ -143,9 +117,13 @@
             // 
             // dgvProveedores
             // 
+            this.dgvProveedores.AllowUserToAddRows = false;
+            this.dgvProveedores.AllowUserToDeleteRows = false;
             this.dgvProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProveedores.ContextMenuStrip = this.cmsMenu;
             this.dgvProveedores.Location = new System.Drawing.Point(301, 12);
             this.dgvProveedores.Name = "dgvProveedores";
+            this.dgvProveedores.ReadOnly = true;
             this.dgvProveedores.Size = new System.Drawing.Size(477, 342);
             this.dgvProveedores.TabIndex = 47;
             // 
@@ -159,19 +137,86 @@
             this.btnCerrar.UseVisualStyleBackColor = true;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(88, 355);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 49;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // mtxtbNro_tel_sec
+            // 
+            this.mtxtbNro_tel_sec.Location = new System.Drawing.Point(12, 265);
+            this.mtxtbNro_tel_sec.Mask = "000-000-0000";
+            this.mtxtbNro_tel_sec.Name = "mtxtbNro_tel_sec";
+            this.mtxtbNro_tel_sec.Size = new System.Drawing.Size(153, 20);
+            this.mtxtbNro_tel_sec.TabIndex = 53;
+            // 
+            // mtxtbNro_tel_princ
+            // 
+            this.mtxtbNro_tel_princ.Location = new System.Drawing.Point(12, 212);
+            this.mtxtbNro_tel_princ.Mask = "000-000-0000";
+            this.mtxtbNro_tel_princ.Name = "mtxtbNro_tel_princ";
+            this.mtxtbNro_tel_princ.Size = new System.Drawing.Size(151, 20);
+            this.mtxtbNro_tel_princ.TabIndex = 52;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 51;
+            this.label3.Text = "Numero Telefonico Secundario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 50;
+            this.label4.Text = "Numero Telefonico Principal";
+            // 
+            // cmsMenu
+            // 
+            this.cmsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripEditar,
+            this.toolStripEliminar});
+            this.cmsMenu.Name = "cmsMenu";
+            this.cmsMenu.Size = new System.Drawing.Size(118, 48);
+            // 
+            // toolStripEditar
+            // 
+            this.toolStripEditar.Name = "toolStripEditar";
+            this.toolStripEditar.Size = new System.Drawing.Size(180, 22);
+            this.toolStripEditar.Text = "Editar";
+            this.toolStripEditar.Click += new System.EventHandler(this.toolStripEditar_Click);
+            // 
+            // toolStripEliminar
+            // 
+            this.toolStripEliminar.Name = "toolStripEliminar";
+            this.toolStripEliminar.Size = new System.Drawing.Size(180, 22);
+            this.toolStripEliminar.Text = "Eliminar";
+            this.toolStripEliminar.Click += new System.EventHandler(this.toolStripEliminar_Click);
+            // 
             // frmProveedores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 401);
+            this.Controls.Add(this.mtxtbNro_tel_sec);
+            this.Controls.Add(this.mtxtbNro_tel_princ);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.dgvProveedores);
             this.Controls.Add(this.txtbEmail);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtbNro_tel_sec);
-            this.Controls.Add(this.txtbNro_tel_princ);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtbDireccion);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.txtbTipo_producto);
@@ -179,9 +224,11 @@
             this.Controls.Add(this.txtbEmpresa);
             this.Controls.Add(this.label2);
             this.Name = "frmProveedores";
-            this.Text = "frmProveedores";
+            this.Text = "Proveedores";
+            this.Activated += new System.EventHandler(this.frmProveedores_Activated);
             this.Load += new System.EventHandler(this.frmProveedores_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).EndInit();
+            this.cmsMenu.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,13 +242,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtbDireccion;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox txtbNro_tel_sec;
-        private System.Windows.Forms.TextBox txtbNro_tel_princ;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtbEmail;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.DataGridView dgvProveedores;
         private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.MaskedTextBox mtxtbNro_tel_sec;
+        private System.Windows.Forms.MaskedTextBox mtxtbNro_tel_princ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip cmsMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripEditar;
+        private System.Windows.Forms.ToolStripMenuItem toolStripEliminar;
     }
 }
--- Supermercado/View/frmProveedores.cs
+++ Supermercado/View/frmProveedores.cs
@@ -1,18 +1,40 @@
-﻿using System;
+﻿using Supermercado.Data;
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace Supermercado
 {
     public partial class frmProveedores : Form
     {
+
+        Datos datos = new Datos();
+        int id = -1;
+
         public frmProveedores()
         {
             InitializeComponent();
         }
 
+        public frmProveedores(int id)
+        {
+            InitializeComponent();
+
+            Datos data = new Datos();
+            DataSet ds = data.getAllData("SELECT * FROM \"proveedores\" WHERE id = " + id);
+            txtbEmpresa.Text = ds.Tables[0].Rows[0]["empresa"].ToString();
+            txtbTipo_producto.Text = ds.Tables[0].Rows[0]["tipo_producto"].ToString();
+            txtbDireccion.Text = ds.Tables[0].Rows[0]["direccion"].ToString();
+            mtxtbNro_tel_princ.Text = ds.Tables[0].Rows[0]["nro_tel_princ"].ToString();
+            mtxtbNro_tel_sec.Text = ds.Tables[0].Rows[0]["nro_tel_sec"].ToString();
+            txtbEmail.Text = ds.Tables[0].Rows[0]["email"].ToString();
+            this.id = id;
+            btnAgregar.Text = "Actualizar";
+        }
+
         private void frmProveedores_Load(object sender, EventArgs e)
         {
-
+            mostrarDatos();
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
@@ -20,6 +42,140 @@ namespace Supermercado
             frmMenu menu = new frmMenu();
             menu.Show();
             this.Hide();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            bool resultado;
+            Datos datos = new Datos();
+
+            if (id == -1)
+            {
+                string queryUpdate = "INSERT INTO public.\"proveedores\" " +
+                    "(empresa, tipo_producto, direccion, nro_tel_princ, nro_tel_sec, email) VALUES (" +
+                    "'" + txtbEmpresa.Text + "'," +
+                    "'" + txtbTipo_producto.Text + "'," +
+                    "'" + txtbDireccion.Text + "'," +
+                    "'" + mtxtbNro_tel_princ.Text + "'," +
+                    "'" + mtxtbNro_tel_sec.Text + "'," +
+                    "'" + txtbEmail.Text + "')";
+
+
+                resultado = datos.ExecuteQuery(queryUpdate);
+                if (resultado)
+                {
+                    MessageBox.Show("Registro agregado con éxito", "Siste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    limpiar();
+
+                }
+                else
+                {
+                    MessageBox.Show("Error al agregar los datos", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            else
+            {
+                string queryup = "UPDATE public.\"proveedores\" SET " +
+                    "empresa = '" + txtbEmpresa.Text + "', " +
+                    "tipo_producto = '" + txtbTipo_producto.Text + "', " +
+                    "direccion = '" + txtbDireccion.Text + "', " +
+                    "nro_tel_princ = '" + mtxtbNro_tel_princ.Text + "', " +
+                    "nro_tel_sec = '" + mtxtbNro_tel_sec.Text + "', " +
+                    "email = '" + txtbEmail.Text + "' " +
+                    "WHERE id = " + id;
+
+                resultado = datos.ExecuteQuery(queryup);
+
+                if (resultado)
+                {
+                    MessageBox.Show("Actualizado con éxito", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("Error al actualizar el registro", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void limpiar()
+        {
+            txtbEmpresa.Text = "";
+            txtbTipo_producto.Text = "";
+            txtbDireccion.Text = "";
+            mtxtbNro_tel_princ.Text = "";
+            mtxtbNro_tel_sec.Text = "";
+            txtbEmail.Text = "";
+        }
+
+        private void mostrarDatos()
+        {
+            DataSet ds = datos.getAllData("SELECT id as \"Id\", " +
+                "empresa as \"Empresa\", " +
+                "tipo_producto as \"Tipo de Producto\"," +
+                "direccion as \"Direccion\", " +
+                "nro_tel_princ as \"Numero Telefonico Principal\", " +
+                "nro_tel_sec as \"Numero Telefonico Secundario\", " +
+                "email \"Email\" FROM \"proveedores\" order by id");
+            if (ds != null)
+            {
+                dgvProveedores.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                MessageBox.Show("No se pudieron obtener los datos de la base de datos.", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmProveedores_Activated(object sender, EventArgs e)
+        {
+            mostrarDatos();
+        }
+
+        private void toolStripEditar_Click(object sender, EventArgs e)
+        {
+            int Id = Convert.ToInt32(dgvProveedores[0, dgvProveedores.CurrentCell.RowIndex].Value);
+            cargarDatos(Id);
+        }
+
+        private void toolStripEliminar_Click(object sender, EventArgs e)
+        {
+            string r = dgvProveedores[0, dgvProveedores.CurrentCell.RowIndex].Value.ToString();
+            DialogResult dr = MessageBox.Show("¿Está seguro de eliminar el registro seleccionado?", "Sistema", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dr == DialogResult.Yes)
+            {
+                bool resultado = datos.ExecuteQuery("DELETE FROM public.\"proveedores\" WHERE id = " + r);
+                if (resultado)
+                {
+                    MessageBox.Show("Registro eliminado con éxito", "Siste", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    mostrarDatos();
+                }
+                else
+                {
+                    MessageBox.Show("Error al eliminar el registro", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void cargarDatos(int clienteId)
+        {
+            DataSet ds = datos.getAllData("SELECT * FROM \"proveedores\" WHERE id = " + clienteId);
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                txtbEmpresa.Text = ds.Tables[0].Rows[0]["empresa"].ToString();
+                txtbTipo_producto.Text = ds.Tables[0].Rows[0]["tipo_producto"].ToString();
+                txtbDireccion.Text = ds.Tables[0].Rows[0]["direccion"].ToString();
+                mtxtbNro_tel_princ.Text = ds.Tables[0].Rows[0]["nro_tel_princ"].ToString();
+                mtxtbNro_tel_sec.Text = ds.Tables[0].Rows[0]["nro_tel_sec"].ToString();
+                txtbEmail.Text = ds.Tables[0].Rows[0]["email"].ToString();
+
+                id = clienteId;
+            }
+            else
+            {
+                MessageBox.Show("No se pudo cargar el cliente.", "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
